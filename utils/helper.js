654aa1093609ac47513f9286fb5073ebd892a21e
Lisa Barton <lisabarton23@yahoo.com>
2021-05-30 14:25:25 -0600

    added blogRoutes.js the coding should be complete for post and delete

--- utils/helper.js
+++ utils/helper.js
@@ -1,1 +1,11 @@
 //will want to add dates to entries formated if time allows
+module.exports = {
+    format_date: (date) => {
+      // Format date as MM/DD/YYYY
+      return date.toLocaleDateString();
+    },
+    format_amount: (amount) => {
+      // format large numbers with commas
+      return parseInt(amount).toLocaleString();
+    },
+}
