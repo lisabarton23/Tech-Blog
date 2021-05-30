f9fb423938a4fbeb29cd5395647816daf0c1206c
Lisa Barton <lisabarton23@yahoo.com>
2021-05-30 14:50:48 -0600

    added all routes to the home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Project, User, Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
 
@@ -9,25 +9,76 @@ try {
 const blogData = await Blog.findAll ({
 include : [
 //this will be the model user and attributes after created
+{model: User,
+    attributes:['name']
+    //add in other data after test this works
+},
+],
 
+});
+const blogs = blogData.map ((blog) => blog.get({plain:true}))
+res.render('homepage', {
+blogs,
+logged_in :req.session.logged_in
+});}
+catch (err) {
+    res.status (500).json(err);
+}});
 
+//try to get blog by user id
+
+router.get('/blog/:id', async (req, res)=> {
+try{
+const blogData = await Blog.findbyPK(req.params.id, {
+    include:[
+{ model:User,
+attributes :['name'],
+},
 ]
+});
 
-})
+const blog =blogData.get ({plain: true});
+res.render('blog', {
+...blog,
+logged_in: req.session.logged_in
+});
 
-
-
-}
-
-
-
-
-
-})
-
+}catch (err) {
+    res.status(500).json(err);
+}});
 
 
-
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+     
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Blog }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
+  
 
 
 
