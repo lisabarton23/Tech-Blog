f483ebe7286984b7dc43eb32832ba8df6ded377b
Lisa Barton <lisabarton23@yahoo.com>
2021-06-08 14:53:10 -0600

    updating

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 let sequelize
 if (process.env.JAWSDB_URL) {
 
-  sequelize = new sequelize(process.env.JAWSDB_URL)
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
 const sequelize = new Sequelize(
   process.env.DB_NAME,
